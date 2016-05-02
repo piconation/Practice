5d7423ebd58f8e38132bd5ed08180043008c33fa
mattpowell <piconation@yahoo.com>
2016-05-02 10:20:03 -0600

    5.2.16

---
+++ js/bizcard.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mattpowell on 5/2/16.
+ */
+
+var bizCard = {name:"Matt Powell", address:"123 Main St", phone:"801-814-0599", email:"mnjpowell07@gmail.com", job:"Sushi Destroyer"};
+
+function card() {
+    document.getElementById("name").innerHTML = bizCard.name;
+    document.getElementById("job").innerHTML = bizCard.job;
+    document.getElementById("address").innerHTML = bizCard.address;
+    document.getElementById("phone").innerHTML = bizCard.phone;
+    document.getElementById("email").innerHTML = bizCard.email
+}
+
--- js/js.js
+++ js/js.js
@@ -1,1 +1,6 @@
-document.getElementById('myexternaldiv').innerHTML = 'BOO YAH!';
+//document.getElementById('myexternaldiv').innerHTML = 'BOO YAH!';
+
+
+var car = {make:"toyota", model:"camry", color:"silver", year:2015, trim:"ce", lift:"none"};
+
+
