6309f665cc1be9be0479af775ae70adc7543c585
mattpowell <piconation@yahoo.com>
2016-05-02 21:48:49 -0600

    5.2.16pm

---
+++ js/factorial.js
@@ -0,0 +1,17 @@
+/**
+ * Created by mattpowell on 5/2/16.
+ */
+
+function factorial(fish) {
+
+    if (fish < 0) {
+        return -1;
+    }
+
+    else if (fish == 0) {
+        return 1;
+    }
+
+    return fish * factorial(fish-1);
+
+}
