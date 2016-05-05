27405b52aea87f619f3245e4e7da7c9114eaa6a5
mattpowell <piconation@yahoo.com>
2016-05-05 10:01:05 -0600

    5.5.16

---
+++ loops.js
@@ -0,0 +1,48 @@
+/**
+ * Created by mattpowell on 5/5/16.
+ */
+
+
+//for loop
+
+for (var i = 0; i < 10; i ++) {
+    console.log(i);
+}
+
+var array = [1,2,3,4,5,6,7,8,9.10];
+
+for (var i = 0; i < array.length; i++) {
+    console.log('index: ' + i + ' content: ' + array[i]);
+}
+
+//for each loop, calls ur function with 
+
+listOfNumbers.forEach(function () {
+    
+})
+
+//for in loop: used for objects
+
+for (var number in ) {
+    console.log(number);
+}
+
+var car = {
+    engine: {
+        belt: 'napabelt',
+        sparkplugs: ['plug', 'plug', 'plug', 'plug']
+    },
+    wheel: {
+        horn: 'loud'
+    }
+};
+
+for (var part in car) {
+    console.log(part);
+}
+
+//while loop
+
+//do while loop
+
+//
