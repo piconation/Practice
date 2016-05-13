0f3c3fde4e63f82e87ac906d05d79d0d7daa234f
mattpowell <piconation@yahoo.com>
2016-05-13 10:38:47 -0600

    5.13.16

---
+++ js/challenge.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mattpowell on 5/13/16.
+ */
+
+
+var number = prompt("Enter your numbers")
+
+var a = [];
+var b = [];
+
+function userEntry(a, b) {
+    return a * b;
+}
+document.getElementById("prompt").innerHTML = answer;
+
+
