0ab40db0fb1747beb5152ac159bce453f2bc327d
mattpowell <piconation@yahoo.com>
2016-05-20 10:14:46 -0600

    5.20.16b

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,25 @@
 /**
  * Created by mattpowell on 5/20/16.
  */
+
+var teetime = 30;
+var seconds = 60;
+
+function beginTimer(){
+    var thetimer = setInterval(function(){clocktick()}, 1000);
+}
+
+function clocktick(){
+    if(seconds > 0){
+        seconds--;
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
+        else {
+            teetime--;
+            seconds = 59;
+        }
+        console.log
+        document.getElementById("countdown").innerHTML = teetime + ":" + seconds;
+    }
+}
