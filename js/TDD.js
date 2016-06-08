1ee9041aee55a2fcaf7a1fcf8220afe524f2f8f9
mattpowell <piconation@yahoo.com>
2016-06-08 10:27:54 -0600

    6.8.16

--- js/TDD.js
+++ js/TDD.js
@@ -2,7 +2,7 @@
  * Created by mattpowell on 5/26/16.
  */
 
-function receive(mynumber) {
+/*function receive(mynumber) {
     if(mynumber >= 1 && mynumber <= 20) {
         return true;
     }
@@ -120,5 +120,33 @@ function LinkedList() {
 function Node(value) {
     this.value = value;
     this.next = null;
+}-*/
+
+function LinkedLists() {
+    this.head = null;
 }
 
+LinkedLists.prototype.push = function (val) {
+    var node = {
+        value: val,
+        next: null
+    }
+    if(!this.head){
+        this.head = node;
+    }
+    else{
+        current = this.head;
+        while(current.next){
+            current = current.next;
+        }
+        current.next = node;
+    }
+}
+
+var myLL = new LinkedLists();
+
+myLL.push(2);
+myLL.push(3);
+myLL.push(4);
+
+console.log(myLL);
